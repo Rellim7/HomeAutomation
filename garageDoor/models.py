ba688c3fc3287b4191578c24db311b706d5303f1
Rellim7 <justin.miller@miraclechannel.ca>
2016-08-26 17:52:13 -0600

    model updates

--- garageDoor/models.py
+++ garageDoor/models.py
@@ -3,3 +3,12 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class door(models.Model):
+    doorName = modelsCharFeild(maxlength = 200)
+
+
+class openEvent(models.Model):
+
+
+class closeEvent(models.Model):
+    
