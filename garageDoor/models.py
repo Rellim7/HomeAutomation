95cdc5c6cd20c8495919a37fb4d27d0160b05d50
Rellim7 <justin.miller@miraclechannel.ca>
2016-08-28 13:33:57 -0600

    model updates

--- garageDoor/models.py
+++ garageDoor/models.py
@@ -4,11 +4,20 @@ from django.db import models
 
 # Create your models here.
 class door(models.Model):
-    doorName = modelsCharFeild(maxlength = 200)
+    doorName = models.CharFeild(maxlength = 200)
+    doorID = models.AutoField(primary_key = True)
+    currentState = models.CharFeild(max_length = 50)
+    lastChanged = models.DateTimeField()
 
 
 class openEvent(models.Model):
-
+    user = models.ForeignKey()
+    openID = models.AutoFeild(primary_key = True)
+    openTime =
+    door = models.ForeignKey()
 
 class closeEvent(models.Model):
-    
+    user= models.ForeignKey()
+    closeTime = models.DateTimeField()
+    closeID = models.AutoField(primary_key = True)
+    door = models.ForeignKey()
