bfe7f41c71fb0d2cbba0549529c56a8d05d9831a
Rellim7 <justin.miller@miraclechannel.ca>
2017-02-17 22:34:05 -0700

    open door

--- garageDoor/views.py
+++ garageDoor/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from models import door, openEvent, closeEvent
+from services import controller
 # Create your views here.
 def index(request):
-    return Httpresponse("Welcome To Prime Ingennuitys Grage door division")
+    return Httpresponse("Welcome To Prime Ingenuitys Garage door division")
+
+def openDoor(request):
+    doorObj = door.objects.filter(doorName="home")
+    controller(doorObj,'rellim')
+    return HttpResponse("door has been opened!")
+
+def closeDoor(request):
+    return
