1e26356c06de6159849b513dd0b6e80bc1305a37
Rellim7 <justin.miller@miraclechannel.ca>
2017-02-17 22:48:21 -0700

    fix

--- garageDoor/urls.py
+++ garageDoor/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'open^$', views.openDoor, name='open'),
+    url(r'^open$', views.openDoor, name='open'),
 ]
